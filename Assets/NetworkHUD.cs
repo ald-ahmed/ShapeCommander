e96e437ecccfb3cdd16e0c64ec3e64a470b72b16
Chris Horrigan <cdhorrigan@gmail.com>
2019-11-09 16:30:40 -0800

    Made a basic networked scene with GUI for connecting and starting the game

---
+++ Assets/NetworkHUD.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkHUD : MonoBehaviour
+{
+
+    [SerializeField]
+    private UIClickable m_joinButton;
+
+    [SerializeField]
+    private UIClickable m_hostButton;
+
+    private NetworkManager m_networkManager;
+
+    
+    
+    void Start()
+    {
+        m_networkManager = gameObject.GetComponent<NetworkManager>();
+        m_joinButton.clickHandler += StartConnecting;
+        m_hostButton.clickHandler += StartHosting;
+        
+    }
+
+ 
+
+    public void StartHosting()
+    {
+        //m_networkManager.networkAddress= "10.26.101.236";
+        m_networkManager.StartHost();
+        Debug.Log("Starting hosting on "+m_networkManager.networkAddress);
+        
+    }
+
+    public void StartConnecting()
+    {
+        
+        m_networkManager.StartClient();
+        Debug.Log("Starting connecting to "+m_networkManager.networkAddress);
+    }
+
+
+}
---
+++ Assets/PlayerFace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFace : MonoBehaviour
+{
+    private Transform m_target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_target = GameObject.Find("Main Camera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_target != null)
+        {
+            transform.position = m_target.position;
+            transform.rotation = m_target.rotation;
+        }
+    }
+}

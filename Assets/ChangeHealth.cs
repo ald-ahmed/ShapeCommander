748023edaceaee6d2246196bf20d7bbb1b415420
Ahmed <aldulaim@usc.edu>
2019-11-19 15:51:37 -0800

    see "networkedscene 1"

---
+++ Assets/ChangeHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class ChangeHealth : MonoBehaviour
+{
+
+
+    public Image myImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myImage = GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void setHealth(float newHealth)
+    {
+        myImage.fillAmount = newHealth;
+    }
+
+
+}
